36011d3702ad818098c41171862ea82dada9a615
David Sng <davidsnghs@gmail.com>
2018-10-31 23:19:40 +0800

    Updating database during deployment

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const Clarifai = require('clarifai');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'davidsng',
-        password: '',
-        database: 'smart-brain'
+        host: process.env.DATABASE_URL,
+        ssl: true,
     }
 })
 
